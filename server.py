174b725a363b797345b1f4b5e6aa0ffb72f7969e
Tom Henderson <tomhenderson@mac.com>
2018-08-09 11:18:21 +1200

    Added logging

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ import logging
 import argparse
 import urllib2
 
-# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(filename='logging.log',level=logging.DEBUG)
 
 # parsing arguments
 PARSER = argparse.ArgumentParser(description='Client message processor')
@@ -52,6 +52,8 @@ def process_message(msg):
     """
     processes the messages by combining and appending the kind code
     """
+    APP.logger.debug("Processing a msg_id %s" % msg['Id'])
+
     msg_id = msg['Id'] # The unique ID for this message
     total_parts = msg['TotalParts']
     part_number = msg['PartNumber'] # Which part of the message it is
@@ -69,8 +71,7 @@ def process_message(msg):
 
     # if both parts are filled, the message is complete
     if None not in parts:
-        # app.logger.debug("got a complete message for %s" % msg_id)
-        print "have both parts"
+        APP.logger.debug("got a complete message for %s" % msg_id)
         # We can build the final message.
         result = ''.join(parts)
         # sending the response to the score calculator
@@ -81,12 +82,11 @@ def process_message(msg):
         APP.logger.debug("ID: %s" % msg_id)
         APP.logger.debug("RESULT: %s" % result)
         url = API_BASE + '/' + msg_id
-        print url
-        print result
+        APP.logger.debug("Sending response to %s" % url)
         req = urllib2.Request(url, data=result, headers={'x-gameday-token':ARGS.API_token})
         resp = urllib2.urlopen(req)
         resp.close()
-        print response
+        APP.logger.debug(resp)
 
     return 'OK'
 
@@ -95,6 +95,7 @@ if __name__ == "__main__":
     # By default, we disable threading for "debugging" purposes.
     # This will cause the app to block requests, which means that you miss out on some points,
     # and fail ALB healthchecks, but whatever I know I'm getting fired on Friday.
+    APP.logger.debug("Starting application")
     APP.run(host="0.0.0.0", port=80)
     
     # Use this to enable threading:
